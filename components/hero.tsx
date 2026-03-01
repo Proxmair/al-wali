e8744c659beabb0ab02ebc4f5fd3f547d7308810
Proxmair <umair.xmair@gmail.com>
2026-03-01 15:12:49 +0500

    fix: navbar responsive ness

--- components/hero.tsx
+++ components/hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
     <section className='relative h-250 overflow-hidden bg-[url("/slider-1.jpeg")] bg-cover bg-no-repeat bg-center flex items-center justify-center'>
       {/* Content */}
       <div className="relative z-10 xl:left-[20vw] flex flex-col justify-center xl:items-start px-4 max-w-3xl mr-3 bg-amber-100 rounded-2xl p-4">
-        <h1 className="text-6xl xl:text-8xl font-bold text-orange-900 mb-4 text-balance text-center xl:text-start">
+        <h1 className="text-5xl sm:text-6xl xl:text-8xl font-bold text-orange-900 mb-4 text-balance text-center xl:text-start">
           Andaz Apka Mehak Hamari
         </h1>
         <p className="text-md xl:text-lg text-foreground/80 mb-8 text-balance font-bold text-center xl:text-start">
@@ -17,15 +17,15 @@ export default function Hero() {
           Ramazan Special offer Buy 3 in pay of 2
         </p>
 
-        <div className="flex flex-col sm:flex-row gap-4 justify-center mx-auto">
-          <Button 
-            size="lg"
+        <div className="w-40 flex sm:flex-row gap-4 justify-center mx-auto">
+          <Button
+            size={'default'}
             className="bg-primary hover:bg-primary/90 text-primary-foreground font-semibold"
           >
             Shop Now
           </Button>
           <Button 
-            size="lg"
+            size={'default'}
             variant="outline"
             className="border-2 border-foreground text-foreground hover:bg-foreground hover:text-background font-semibold"
           >

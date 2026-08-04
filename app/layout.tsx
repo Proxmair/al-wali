abc504b8b0cd2f10c8c9506901d1c85f69719564
Proxmair <umair.xmair@gmail.com>
2026-08-05 01:07:12 +0500

    feat: add meta interaction

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ReduxProvider from '@/store/provider'
 const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 import WhatsAppButton from '@/components/WhatsAppButton'
+import Script from 'next/script'
 export const metadata: Metadata = {
   title: 'AL Wali - Premium Perfumes | Andaz Apka Mehak Humari',
   description: 'Discover exquisite fragrances at AL Wali. Premium quality perfumes with special deals and authentic products.',
@@ -37,6 +38,31 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans antialiased">
+        <Script id="facebook-pixel" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1403934108326992');
+    fbq('track', 'PageView');
+  `}
+</Script>
+
+<noscript>
+  <img
+    height="1"
+    width="1"
+    style={{ display: "none" }}
+    src="https://www.facebook.com/tr?id=1403934108326992&ev=PageView&noscript=1"
+    alt=""
+  />
+</noscript>
         <ReduxProvider>
           {children}
           <WhatsAppButton />
